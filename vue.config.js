05a19d0138e74bb6d459ddaa387ce6402137949c
真心先生 <pmb2020@163.com>
2021-01-27 14:53:22 +0800

    冲突

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
 	productionSourceMap: false,//编译时不生成map文件，减少文件体积
+	publicPath: process.env.NODE_ENV === 'production' ?
+		'./' :
+		'/'
+	,
 	devServer: {
 		open: true,
 		port: 9090
 	},
-	publicPath: process.env.NODE_ENV === 'production' ?
-		'./' :
-		'/'
 }

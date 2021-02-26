78afd33bbab776073b328fe009dd8b50b2297a1f
wangbo <pmb2020@163.com>
2021-02-26 21:35:08 +0800

    文章列表

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="article">
 		<el-tabs v-model="activeName" @tab-click="handleClick">
-			<el-tab-pane label='全部(42)' name="first"></el-tab-pane>
-			<el-tab-pane label="待审核(4)" name="second"></el-tab-pane>
-			<el-tab-pane label="草稿箱(2)" name="third"></el-tab-pane>
+			<el-tab-pane label='全部(42)' name="1"></el-tab-pane>
+			<el-tab-pane label="待审核(4)" name="2"></el-tab-pane>
+			<el-tab-pane label="草稿箱(2)" name="3"></el-tab-pane>
 		</el-tabs>
 		<!-- <p>{{activeName}}</p> -->
 		<ul class="list_ul">
@@ -12,8 +12,8 @@
 				<div class="list_info">
 					<div class="list_info_left">
 						<span>原创</span>
-						<span>{{item.type}}</span>
-						<span>{{item.time}}</span>
+						<span>{{item.keywords}}</span>
+						<span>{{item.created_at}}</span>
 						<span><i class="el-icon-view"></i>{{item.read_num}}</span>
 					</div>
 					<div class="list_info_right">
@@ -23,7 +23,7 @@
 				</div>
 			</li>
 		</ul>
-		<el-pagination background layout="prev, pager, next" :total="100">
+		<el-pagination background layout="prev, pager, next" :total="total" @current-change="handleCurrentChange" :page-size="pageSize">
 		</el-pagination>
 	</div>
 </template>
@@ -33,20 +33,30 @@
 	export default {
 		data() {
 			return {
-				activeName: 'first',
+				activeName: '1',
+				total:1,
+				page:1,
+				pageSize:15,
 				articleList:[],
 			};
 		},
 		mounted() {
-			article('GET',{id:'455'}).then(res=>{
-				// console.log(res.data)
-				this.articleList=res
-			})
+			this.getArticle(this.page)
 		},
 		methods: {
 			handleClick(tab, event) {
-				console.log(tab.label);
+				this.getArticle(this.activeName)
 				// this.activeName='sad'+tab.label
+			},
+			handleCurrentChange(val){
+				this.page=val
+				this.getArticle(this.page)
+			},
+			getArticle(page){
+				article('GET',{page:page}).then(res=>{
+					this.articleList=res.data
+					this.total=res.total
+				})
 			}
 		}
 	};

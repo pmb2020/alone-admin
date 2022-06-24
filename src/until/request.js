21c1ec68e03c2ef754f9844b35a0fb37e0d8c3d6
WangBo <pmb2020@163.com>
2022-06-24 10:09:50 +0800

    axios封装优化

--- src/until/request.js
+++ src/until/request.js
@@ -1,21 +1,18 @@
 import axios from 'axios'
-import qs from 'qs';
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css';
+
 const instance = axios.create({
 	baseURL:'https://alone.usemock.com/api/',
-	timeout: 1000,
-	headers: {
-		'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-	},
+	timeout: 5000,
 })
 
 // 响应拦截器
 instance.interceptors.response.use(function(response){
 	if(response.data.code != 0){
-		ElMessage.error(response.data.msg)
+		ElMessage.error(response.data.code+'：'+response.data.msg)
 	}
-	return response.data
+	return response.data.data
 },function(error){
 	return Promise.reject(error)
 })
@@ -35,7 +32,7 @@ export function get(url, params = {}) {
 
 export function post(url, data = {}){
 	return new Promise((resolve, reject)=>{
-		instance.post(url,qs.stringify(data)).then(res=>{
+		instance.post(url,data).then(res=>{
 			resolve(res);
 		}).catch(err=>{
 			reject(err)

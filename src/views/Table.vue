b9fc71b2d48ef12b5c6a889c6e5d8238c1e0da60
vaa <pmb2020@vaa.com>
2022-07-06 16:19:16 +0800

    筛选

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,17 +1,22 @@
 <template>
 	<div class="filter-box al-box" style="display: flex;">
-		<el-form :inline="true" :model="filterForm" class="demo-form-inline">
+		<el-form :inline="true" :model="filterForm" class="demo-form-inline" size="large">
 			<el-form-item label="名称">
 				<el-input v-model="filterForm.name" placeholder="要搜索的名称" />
 			</el-form-item>
-			<el-form-item label="类型">
+			<el-form-item label="订单状态">
 				<el-select v-model="filterForm.type" placeholder="请选择">
-					<el-option label="虚拟商品" value="shanghai" />
-					<el-option label="实物商品" value="beijing" />
+					<el-option label="已完成" value="1" />
+					<el-option label="未支付" value="2" />
+					<el-option label="已取消" value="3" />
 				</el-select>
 			</el-form-item>
+			<el-form-item label="日期">
+				<el-date-picker v-model="filterForm.date" type="daterange" unlink-panels range-separator="至"
+					start-placeholder="开始日期" end-placeholder="结束日期" :shortcuts="shortcuts" />
+			</el-form-item>
 			<el-form-item>
-				<el-button type="primary" @click="getList">查询</el-button>
+				<el-button size="default" type="primary" @click="getList">查询</el-button>
 			</el-form-item>
 		</el-form>
 	</div>
@@ -39,6 +44,13 @@
 			<el-table-column prop="amount" align="center" label="金额" />
 			<el-table-column prop="name" align="center" label="姓名" />
 			<el-table-column prop="phone" align="center" label="手机号" />
+			<el-table-column label="订单状态" align="center">
+				<template #default="scope">
+					<el-tag v-if="scope.row.type ===1" type="success">已完成</el-tag>
+					<el-tag v-else-if="scope.row.type ===2" type="warning">未支付</el-tag>
+					<el-tag v-else type="info">已取消</el-tag>
+				</template>
+			</el-table-column>
 			<el-table-column prop="date" align="center" label="时间" width="165" />
 			<el-table-column label="操作" align="center" width="170">
 				<template #default="scope">
@@ -55,9 +67,9 @@
 		</div>
 	</div>
 	<!-- 弹框表单 -->
-	<el-dialog v-model="dialogFormVisible" title="新增商品" >
+	<el-dialog v-model="dialogFormVisible" title="新增商品">
 		<el-form :model="tableForm" :label-width="80" size="large">
-			<el-form-item label="名称：" >
+			<el-form-item label="名称：">
 				<el-input v-model="tableForm.name" autocomplete="off" />
 			</el-form-item>
 			<el-form-item label="类型：">
@@ -66,10 +78,10 @@
 					<el-option label="实物商品" value="beijing" />
 				</el-select>
 			</el-form-item>
-			<el-form-item label="金额：" >
+			<el-form-item label="金额：">
 				<el-input v-model="tableForm.amount" autocomplete="off" />
 			</el-form-item>
-			<el-form-item label="手机号：" >
+			<el-form-item label="手机号：">
 				<el-input v-model="tableForm.amount" autocomplete="off" />
 			</el-form-item>
 		</el-form>
@@ -93,8 +105,36 @@
 	} from 'vue'
 	const filterForm = reactive({
 		'name': '',
-		'date': '2022-05-03'
+		'date': ''
 	})
+	const shortcuts = [{
+			text: '最近一周',
+			value: () => {
+				const end = new Date()
+				const start = new Date()
+				start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+				return [start, end]
+			},
+		},
+		{
+			text: '最近一月',
+			value: () => {
+				const end = new Date()
+				const start = new Date()
+				start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+				return [start, end]
+			},
+		},
+		{
+			text: '最近三个月',
+			value: () => {
+				const end = new Date()
+				const start = new Date()
+				start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+				return [start, end]
+			},
+		},
+	]
 	const tableForm = reactive({})
 	const dialogFormVisible = ref(false)
 	const tableRef = ref([])
@@ -136,7 +176,8 @@
 	.filter-box .el-form-item {
 		margin-bottom: 0;
 	}
+
 	.el-input {
-	  width: 300px;
+		width: 300px;
 	}
 </style>

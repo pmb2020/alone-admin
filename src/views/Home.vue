3484a15b004923a95fe9fe1ddf8574789babaeee
pmb2020 <pmb2020@vaa.com>
2022-07-11 18:02:14 +0800

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
 	<div>
-		<el-card class="box-card">
-			<div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
-		</el-card>
-		<el-button type="primary">Primary{{store.count}}</el-button>
+		<el-row :gutter="40">
+			<el-col :xs="8" :sm="6" :lg="4">
+				<div class="al-icon-box al-bg1">dd</div>
+			</el-col>
+			<el-col :xs="8" :sm="6" :lg="4">
+				<div class="al-icon-box" style="background-color: #E5F8EB">dd</div>
+			</el-col>
+			<el-col :xs="8" :sm="6" :lg="4" style="background-color: rgba(60, 33, 247, 0.1)">
+				<div class="al-icon-box">
+					ff
+				</div>
+			</el-col>
+			<el-col :xs="8" :sm="6" :lg="4" style="background: #3C21F7;">
+				<div class="al-icon-box">dd</div>
+			</el-col>
+			<el-col :xs="8" :sm="6" :lg="4" style="background: #E6FFFF;">
+				<div class="al-icon-box">dd</div>
+			</el-col>
+			<el-col :xs="8" :sm="6" :lg="4" style="background: #EAEFFF;">
+				<div class="al-icon-box">dd</div>
+			</el-col>
+		</el-row>
 	</div>
 </template>
 
@@ -28,4 +46,16 @@
 </script>
 
 <style>
+	.al-icon-box{
+		padding: 25px 20px;
+		border-radius: 10px;
+		cursor: pointer;
+		position: relative;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.al-bg1{
+		background-color: rgba(255, 188, 3, 0.1);
+		/* box-shadow: 0 9px 9px -1px rgb(255 188 3 / 30%); */
+	}
 </style>

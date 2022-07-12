d7d149a745ff34193c257e7f252429243c5ecc88
pmb2020 <pmb2020@vaa.com>
2022-07-12 18:00:51 +0800

    al-icon-box

---
+++ src/components/alone/IconBox.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="al-icon-box al-bg2" :style="{backgroundColor:props.bgColor}">
+		<div class="icon-bg">
+			<el-icon color="#fff" :size="24"><CameraFilled /></el-icon>
+		</div>
+		<div class="icon-text">
+			<p style="margin-bottom: 10px;">{{props.name}}</p>
+			<p style="color: #FFBB01;font-size: 22px;">425</p>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	const props = defineProps({
+		bgColor:{
+			type:String,
+			default:'#fff'
+		},
+		name:{
+			type:String,
+			default:'无'
+		}
+	})
+	console.log(props.bgColor)
+</script>
+
+<style scoped>
+	.al-icon-box{
+		padding: 25px 20px;
+		border-radius: 10px;
+		margin-bottom: 15px;
+		cursor: pointer;
+		position: relative;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.al-bg1{
+		background-color: rgba(255, 188, 3, 0.1);
+		/* box-shadow: 0 9px 9px -1px rgb(255 188 3 / 30%); */
+	}
+	.al-bg2{
+		background-color: #E5F8EB;
+		/* box-shadow: 0 9px 9px -1px rgb(255 188 3 / 30%); */
+	}
+	.icon-bg{
+		width: 50px;
+		height: 50px;
+		border-radius: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background: linear-gradient(to bottom, #FFBB00 0%, #FFF574 100%);
+	}
+	.icon-text{
+		margin-left: 15px;
+		font-size: 15px;
+		font-weight: 600;
+		color: #222943;
+	}
+	.icon-text p{
+		margin: 0;
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,61 +1,28 @@
 <template>
 	<div>
 		<el-row :gutter="40">
-			<el-col :xs="8" :sm="6" :lg="4">
-				<div class="al-icon-box al-bg1">dd</div>
-			</el-col>
-			<el-col :xs="8" :sm="6" :lg="4">
-				<div class="al-icon-box" style="background-color: #E5F8EB">dd</div>
-			</el-col>
-			<el-col :xs="8" :sm="6" :lg="4" style="background-color: rgba(60, 33, 247, 0.1)">
-				<div class="al-icon-box">
-					ff
-				</div>
-			</el-col>
-			<el-col :xs="8" :sm="6" :lg="4" style="background: #3C21F7;">
-				<div class="al-icon-box">dd</div>
-			</el-col>
-			<el-col :xs="8" :sm="6" :lg="4" style="background: #E6FFFF;">
-				<div class="al-icon-box">dd</div>
-			</el-col>
-			<el-col :xs="8" :sm="6" :lg="4" style="background: #EAEFFF;">
-				<div class="al-icon-box">dd</div>
-			</el-col>
+			<template v-for="(item,index) in boxLists" :key="index">
+				<el-col :xs="8" :sm="6" :lg="4">
+					<IconBox :bg-color="item.bgColor" :name="item.name"></IconBox>
+				</el-col>
+			</template>
 		</el-row>
 	</div>
 </template>
 
-<script>
-	import {get} from '@/until/request.js'
-	import {getUser,login} from '@/api/user.js'
-	import {useStore} from '@/store/index'
-	export default{
-		setup(){
-			let store = useStore()
-			console.log(store.count)
-			return {
-				store
-			}
-		},
-		mounted() {
-			// getUser({user_id:100555}).then(res=>{
-			// 	console.log(res)
-			// })
-		}
-	}
+<script setup>
+	import IconBox from '@/components/alone/IconBox.vue'
+	import {reactive,ref} from 'vue'
+	const boxLists = reactive([
+		{bgColor:'rgba(255, 188, 3, 0.1)',name:'信息'},
+		{bgColor:'#E5F8EB',name:'客户'},
+		{bgColor:'rgba(60, 33, 247, 0.1)',name:'线索'},
+		{bgColor:'#E6FFFF',name:'老板'},
+		{bgColor:'#FFF2E5',name:'未读'},
+		{bgColor:'#FEE9ED',name:'信息'},
+	])
 </script>
 
 <style>
-	.al-icon-box{
-		padding: 25px 20px;
-		border-radius: 10px;
-		cursor: pointer;
-		position: relative;
-		display: flex;
-		flex-wrap: wrap;
-	}
-	.al-bg1{
-		background-color: rgba(255, 188, 3, 0.1);
-		/* box-shadow: 0 9px 9px -1px rgb(255 188 3 / 30%); */
-	}
+	
 </style>

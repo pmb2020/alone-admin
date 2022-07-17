40ed0e4c81bb655fcc1ae0fb2fc4b92e5994ca52
wangbo <pmb2020@163.com>
2022-07-17 18:35:22 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,38 @@
 <template>
 	<div>
-		<el-row :gutter="40">
+		<el-row :gutter="30">
 			<template v-for="(item,index) in boxLists" :key="index">
 				<el-col :xs="8" :sm="6" :lg="4">
 					<IconBox :bg-color="item.bgColor" :name="item.name" :icon="item.icon"></IconBox>
 				</el-col>
 			</template>
 		</el-row>
-		<el-row :gutter="40" style="margin-top: 10px;">
+		<el-row :gutter="30" style="margin-top: 10px;">
 			<el-col :span="16">
-				<div class="al-box">
-					s
-				</div>
+				<h2 class="card-h">服务器信息</h2>
+				<el-card class="box-card">
+					<table border="1">
+						<tr>
+							<th>Month</th>
+							<th>Savings</th>
+						</tr>
+						<tr>
+							<td>January</td>
+							<td>$100</td>
+						</tr>
+					</table>
+					<el-table :data="systemInfo" style="width: 100%">
+						<el-table-column prop="system期" width="180" />
+						<el-table-column prop="name" label="Name" width="180" />
+						<el-table-column prop="address" label="Address" />
+					</el-table>
+				</el-card>
 			</el-col>
 			<el-col :span="8">
-				<div class="al-box">12</div>
+				<h2 class="card-h">通知</h2>
+				<div class="al-box">
+					<el-empty description="description" />
+				</div>
 			</el-col>
 		</el-row>
 	</div>
@@ -22,17 +40,53 @@
 
 <script setup>
 	import IconBox from '@/components/alone/IconBox.vue'
-	import {reactive,ref} from 'vue'
-	const boxLists = reactive([
-		{bgColor:'rgba(255, 188, 3, 0.1)',name:'信息',icon:'ElemeFilled'},
-		{bgColor:'#E5F8EB',name:'客户',icon:'Drizzling'},
-		{bgColor:'rgba(60, 33, 247, 0.1)',name:'线索',icon:'FolderChecked'},
-		{bgColor:'#E6FFFF',name:'老板',name:'线索',icon:'GoodsFilled'},
-		{bgColor:'#FFF2E5',name:'未读',name:'线索',icon:'IceDrink'},
-		{bgColor:'#FEE9ED',name:'信息',name:'线索',icon:'Lightning'},
+	import {
+		reactive,
+		ref
+	} from 'vue'
+	const boxLists = reactive([{
+			bgColor: '#c8ddf7',
+			name: '信息',
+			icon: 'ElemeFilled'
+		},
+		{
+			bgColor: '#c9f7d8',
+			name: '客户',
+			icon: 'Drizzling'
+		},
+		{
+			bgColor: '#e9ddf9',
+			name: '线索',
+			icon: 'FolderChecked'
+		},
+		{
+			bgColor: '#ffd7db',
+			name: '老板',
+			icon: 'GoodsFilled'
+		},
+		{
+			bgColor: '#FFF2E5',
+			name: '未读',
+			icon: 'IceDrink'
+		},
+		{
+			bgColor: '#c2eafd',
+			name: '信息',
+			icon: 'Lightning'
+		},
+	])
+	const systemInfo = ref([
+		{system:'Linux'},
+		{mysql:'5.7'},
+		{mysql:'5.7'},
 	])
 </script>
 
 <style>
-	
+	.card-h {
+		text-align: left;
+		font-size: 16px;
+		margin: 0 0 10px 0;
+		color: var(--el-text-color-primary);
+	}
 </style>

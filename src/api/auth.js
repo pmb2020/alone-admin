cd79fbb28ef88e166fe312d27cebbc020017871b
pmb2020 <pmb2020@vaa.com>
2022-09-01 10:00:18 +0800

    yarn up

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import {get,post} from '@/utils/request.js'
 
 export const login = (data) =>{
-	return post('login',data)
+	return post('https://alone.usemock.com/api/login',data)
 }
 
 export const loginOut = (data) =>{

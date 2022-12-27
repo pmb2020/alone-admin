f164adeb7ccfb911c2d02156ee6cd4f339204900
pmb2020 <pmb2020@163.com>
2022-12-27 16:07:47 +0800

    侧边导航独立滚动

--- src/layout/BaseSide.vue
+++ src/layout/BaseSide.vue
@@ -1,77 +1,86 @@
 <template>
-	<div>
+	<div class="base-side">
 		<div class="menu-btn" :class="{'menu-btn-collapse':isCollapse}" @click="isCollapse=isCollapse ? false : true">
 			<el-icon :size="25">
 				<Expand v-if="isCollapse" />
 				<Fold v-else />
 			</el-icon>
 		</div>
-		<el-menu :default-active="menuRoute" class="el-menu-vertical-demo" :collapse="isCollapse" router @open="handleOpen" @close="handleClose">
-			<template v-for="(items,index) in menuItems" :key="index">
-				<el-menu-item v-if="items.children && items.children.length == 1" :index="items.path+'/'+items.children[0].path">
-					<el-icon>
-						<component :is="items.children[0].meta.icon"></component>
-					</el-icon>
-					<span>{{items.children[0].meta.title}}</span>
-				</el-menu-item>
-				<el-sub-menu v-else :index="items.path">
-					<template #title>
+		<el-scrollbar class="al-scrollbar" height="100vh">
+			<el-menu :default-active="menuRoute" class="el-menu-vertical-demo" :collapse="isCollapse" router
+				@open="handleOpen" @close="handleClose">
+				<template v-for="(items,index) in menuItems" :key="index">
+					<el-menu-item v-if="items.children && items.children.length == 1"
+						:index="items.path+'/'+items.children[0].path">
 						<el-icon>
-							<component :is="items.meta.icon" ></component>
+							<component :is="items.children[0].meta.icon"></component>
 						</el-icon>
-						<span>{{items.meta.title}}</span>
-					</template>
-					<el-menu-item-group>
-						<template v-for="(subItem,subIndex) in items.children" :key="subIndex">
-							<el-menu-item v-if="!subItem.hidden" :index="items.path+'/'+subItem.path">
-								{{subItem.meta.title}}
-							</el-menu-item>
+						<span>{{items.children[0].meta.title}}</span>
+					</el-menu-item>
+					<el-sub-menu v-else :index="items.path">
+						<template #title>
+							<el-icon>
+								<component :is="items.meta.icon"></component>
+							</el-icon>
+							<span>{{items.meta.title}}</span>
 						</template>
-					</el-menu-item-group>
-				</el-sub-menu>
-			</template>
-		</el-menu>
+						<el-menu-item-group>
+							<template v-for="(subItem,subIndex) in items.children" :key="subIndex">
+								<el-menu-item v-if="!subItem.hidden" :index="items.path+'/'+subItem.path">
+									{{subItem.meta.title}}
+								</el-menu-item>
+							</template>
+						</el-menu-item-group>
+					</el-sub-menu>
+				</template>
+			</el-menu>
+		</el-scrollbar>
 	</div>
 </template>
 
 <script lang="ts">
-	export default{
-		data(){
-			return{
-				isCollapse:false,
-				menuItems:[],
-				menuRoute:''
+	export default {
+		data() {
+			return {
+				isCollapse: false,
+				menuItems: [],
+				menuRoute: ''
 			}
 		},
-		watch:{
-			isCollapse(n,o){
-				console.log('sss'+n)
+		watch: {
+			isCollapse(n, o) {
+				console.log('sss' + n)
 			}
 		},
 		mounted() {
-			this.menuRoute=this.$route.path
-			this.menuItems = this.$router.options.routes.filter((item)=>{
-				if(item.path=='/'){
-					item.path=''
+			this.menuRoute = this.$route.path
+			this.menuItems = this.$router.options.routes.filter((item) => {
+				if (item.path == '/') {
+					item.path = ''
 				}
 				return !item.hidden
 			})
 		},
-		methods:{
-			handleOpen(){
-				
+		methods: {
+			handleOpen() {
+
 			},
-			handleClose(){
-				
+			handleClose() {
+
 			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+	.base-side{
+		background-color: var(--el-menu-bg-color);
+	}
 	.el-menu-vertical-demo:not(.el-menu--collapse) {
 		width: 240px;
+		height: 100%;
 	}
+
 	.menu-btn {
 		position: absolute;
 		top: 15px;
@@ -85,7 +94,8 @@
 		border-right: none;
 		height: 100%;
 	}
-	.menu-btn-collapse{
+
+	.menu-btn-collapse {
 		left: 70px;
 	}
 </style>
